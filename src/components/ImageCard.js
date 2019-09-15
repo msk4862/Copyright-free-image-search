0a2dcaa4820f962edb29f6724f380814ad966c99
Shoaib <msk4862@gmail.com>
2019-09-16 00:35:30 +0530

    GRID-fixes

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,19 +3,30 @@ import React from 'react'
 class ImageCard extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            spans: 0
+        }
 
         this.imageRef = React.createRef()
     }
 
     componentDidMount() {
-        console.log(this.imageRef)
+        //when image is loaded
+        this.imageRef.current.addEventListener('load', this.setSpan)
     }
+
+    setSpan = () => {
+        console.log(this.imageRef.current.clientHeight)
+        // grid-auto-rows=10 in css
+        const span = Math.ceil(this.imageRef.current.clientHeight / 10)
+        this.setState({spans : span})
+    }
+
 
     render() {
         const {urls, alt_description} = this.props.image
         return (
-            <div>
+            <div style={{gridRowEnd : `span ${this.state.spans}`}}>
                 <img ref={this.imageRef} src={urls.regular} alt={alt_description}/>
             </div>
         )
