30e1ec7bb2735ae4894f315626a4d4b58301191f
msk4862 <msk4862@gmail.com>
2021-01-17 17:13:51 +0530

    added redirection to Image page

--- client-side/src/components/SearchBar.js
+++ client-side/src/components/SearchBar.js
@@ -1,53 +1,64 @@
 import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
 import "../styles/SearchBar.scss";
-import { TITLE, TEXTS } from "../uitilities/Constants";
+import { TITLE, TITLE_SM, TEXTS } from "../uitilities/Constants";
 
-const SearchBar = (props) => {
+const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState("");
+    const [redirect, setRedirect] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        if (searchTerm.trim() !== "")
-            props.onSubmit(searchTerm);
+        setRedirect(true);
     };
 
     const { h1, h2, h2_small_devices } = TEXTS;
     return (
-        <div className="d-flex flex-column search-bar">
-            <h1>{TITLE}</h1>
-            <div className="d-flex desc justify-content-center">
-                <div className="col-10 col-sm-8 flex-column">
-                    <h2>{h1}</h2>
-                    <small className="d-none d-sm-block">{h2}</small>
-                    <small className="d-block d-sm-none">
-                        {h2_small_devices}
-                    </small>
+        <>
+            {redirect && 
+            // redirect to `Images` page
+            <Redirect 
+                to={{
+                    pathname: `/search/${searchTerm}`,
+                }}
+            />}
+            <div className="d-flex flex-column search-bar">
+                <h1 className="d-none d-sm-block">{TITLE}</h1>
+                <h1 className="d-block d-sm-none">{TITLE_SM}</h1>
+                <div className="d-flex desc justify-content-center">
+                    <div className="col-10 col-sm-8 flex-column">
+                        <h2>{h1}</h2>
+                        <small className="d-none d-sm-block">{h2}</small>
+                        <small className="d-block d-sm-none">
+                            {h2_small_devices}
+                        </small>
+                    </div>
                 </div>
-            </div>
 
-            <form className="mb-auto" onSubmit={handleSubmit}>
-                <div className="row justify-content-center m-0">
-                    <input
-                        className="col-10 col-sm-6 form-control"
-                        type="text"
-                        placeholder="Type something to search..."
-                        name="search"
-                        value={searchTerm}
-                        onChange={(evt) => setSearchTerm(evt.target.value)}
-                    />
-                    <button
-                        type="submit"
-                        className="btn search-btn align-items-stretch">
-                        Search
-                    </button>
-                    <button
-                        className="btn search-btn-small"
-                        aria-label="Search">
-                        <i className="fas fa-search"></i>
-                    </button>
-                </div>
-            </form>
-        </div>
+                <form className="mb-auto" onSubmit={handleSubmit}>
+                    <div className="row justify-content-center m-0">
+                        <input
+                            className="col-10 col-sm-6 form-control"
+                            type="text"
+                            placeholder="Type something to search..."
+                            name="search"
+                            value={searchTerm}
+                            onChange={(evt) => setSearchTerm(evt.target.value)}
+                        />
+                        <button
+                            type="submit"
+                            className="btn search-btn align-items-stretch">
+                            Search
+                        </button>
+                        <button
+                            className="btn search-btn-small"
+                            aria-label="Search">
+                            <i className="fas fa-search"></i>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
     );
     
 }
